9daf17c8daa4f3a30e27e18e96e216b2f88acee7
Janice <findjanice@gmail.com>
2015-09-07 16:14:45 -0600

    first commit

---
+++ webpack.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  devtools: 'eval',
+  entry: "./app/App.js",
+  output: {
+    filename: "public/bundle.js"
+  },
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loader: 'jsx-loader'
+    }]
+  }
+};
